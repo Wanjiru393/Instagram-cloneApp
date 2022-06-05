8da5f2112e55d7d597a4e4a29da6b4a95668661c
Wanjiru393 <carolciru393@gmail.com>
2022-06-05 16:49:01 +0300

    Created model views

--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 
+
+# class PostView(ListView):
+
+#     template_name = 'index.html'
+#     queryset =Post.objects.all()
+#     context_object_name = 'posts'
+
 def index(request):
-    
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+
+    context = {'posts':posts}
+    return render(request, 'index.html', context)
+
