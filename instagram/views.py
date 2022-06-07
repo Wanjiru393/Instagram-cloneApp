2d59b73ca08facb599b2377445d0cb4ad73a89fa
Wanjiru393 <carolciru393@gmail.com>
2022-06-07 21:58:38 +0300

    Created post display page

--- instagram/views.py
+++ instagram/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post,Stream
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
-# class PostView(ListView):
-
-#     template_name = 'index.html'
-#     queryset =Post.objects.all()
-#     context_object_name = 'posts'
-
+# @login_required()
 def index(request):
-    posts = Post.objects.all()
-    return render(request, 'instagram/index.html', {'posts': posts})
+    user = request.user
+    # posts = Post.objects.all()
+    posts = Stream.objects.filter()
+
+    group_ids = []
+
+    for post in posts:
+        group_ids.append(post.post_id)
+
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by('-created_date')
+
+    context = {
+        'post_items': post_items,
+    }
+    return render(request, 'instagram/index.html', context )
 
 
-# def create(request):
-#     posts = Post.objects.all()
-#     from_class = PostForm
-#     return render(request,'post_create.html')
 
-#     def form_valid(self,form):
-#         print(form.cleaned_data)
+
 
