dbcb1d56f992c6f346455456a10b204053e55940
Wanjiru393 <carolciru393@gmail.com>
2022-06-09 09:14:52 +0300

    Bug fix

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -172,4 +172,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
